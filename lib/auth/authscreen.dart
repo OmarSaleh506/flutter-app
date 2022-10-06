1e8e63d9012d336320d312b9601e1b0a4300b358
Omar Alwahiby <omaralwahiby@Omars-MacBook-Pro.local>
2022-10-06 15:27:59 +0300

    gg

--- lib/auth/authscreen.dart
+++ lib/auth/authscreen.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:todo/auth/authform.dart';
 
-class AuthScreen extends StatefulWidget {
+class AuthScreen extends StatelessWidget {
   const AuthScreen({super.key});
 
-  @override
-  State<AuthScreen> createState() => _AuthScreenState();
-}
-
-class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("ToDo")),
-      body: AuthForm(),
+      body: AuthScreen(),
     );
   }
 }
